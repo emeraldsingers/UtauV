d287f09a49f408f2701c37c84f8cf2846dbd5a5d
emeraldsingers <zonal9999999@gmail.com>
2026-08-12 20:53:17 +0300

    make Dark Teal standart theme

--- OpenUtau.Core/Util/Preferences.cs
+++ OpenUtau.Core/Util/Preferences.cs
@@ -149,7 +149,7 @@ namespace OpenUtau.Core.Util {
             public int? PlaybackDeviceIndex;
             public bool ShowPrefs = true;
             public bool ShowTips = true;
-            public string ThemeName = "Light";
+            public string ThemeName = "Dark Teal";
             public double ButtonCornerRadius = 0;
             public double NoteCornerRadius = 2;
             public double UiCornerRadius = 0;
