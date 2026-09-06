34d48dcfa6c333966f6ef5f61c9cd91f99f4fcdc
emeraldsingers <zonal9999999@gmail.com>
2026-09-06 13:41:03 +0300

    Expose updater release metadata for tests

--- OpenUtau/ViewModels/UpdaterViewModel.cs
+++ OpenUtau/ViewModels/UpdaterViewModel.cs
@@ -22,13 +22,14 @@ namespace OpenUtau.App.ViewModels {
             public string name = string.Empty;
             public string browser_download_url = string.Empty;
         }
-        class GithubRelease {
+        public class GithubRelease {
 #pragma warning disable 0649
             public string html_url = string.Empty;
             public long id = long.MaxValue;
             public bool draft;
             public bool prerelease;
             public string name = string.Empty;
+            public string tag_name = string.Empty;
             public GithubReleaseAsset[] assets = new GithubReleaseAsset[0];
 #pragma warning restore 0649
         }
